b1fed0c3cff41468d395a3e93b8fe25aff584f1a
AhmadMuzayyin <muzayyin654@gmail.com>
2024-08-27 21:10:44 +0700

    Adding count data on dashboar admin

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,8 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\AspekKriteria;
 use App\Models\Galeri;
+use App\Models\HasilAkhir;
 use App\Models\Kelas;
+use App\Models\Kriteria;
+use App\Models\Penilaian;
 use App\Models\Post;
 use App\Models\Siswa;
 use Illuminate\Http\Request;
@@ -16,6 +20,12 @@ class DashboardController extends Controller
         $siswa = Siswa::all();
         $galeri = Galeri::all();
         $posts = Post::all();
-        return view('home.index', compact('kelas', 'siswa', 'galeri', 'posts'));
+        $aspek = AspekKriteria::get()->count();
+        $kriteria = Kriteria::get()->count();
+        $penilaian = Penilaian::get()->groupBy('siswa_id')->count();
+        $hasilAkhir = HasilAkhir::get()->count();
+        $siswaAlumni = Siswa::where('status_kelulusan', true)->get()->count();
+        // $rekapan = 
+        return view('home.index', compact('kelas', 'siswa', 'galeri', 'posts', 'aspek', 'kriteria', 'penilaian', 'hasilAkhir', 'siswaAlumni'));
     }
 }
--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -2,79 +2,199 @@
 @section('content')
     <!-- Content Row -->
     @if (Auth::user()->level == 'admin')
-    <div class="row">
-        <!-- Kelas Card Example -->
-        <div class="col-xl-3 col-md-6 mb-4">
-            <div class="card border-left-primary shadow h-100 py-2">
-                <div class="card-body">
-                    <div class="row no-gutters align-items-center">
-                        <div class="col mr-2">
-                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
-                                Kelas</div>
-                            <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $kelas->count() }}</div>
-                        </div>
-                        <div class="col-auto">
-                            <i class="fas fa-house fa-2x text-gray-300"></i>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <!-- Siswa Card Example -->
-        <div class="col-xl-3 col-md-6 mb-4">
-            <div class="card border-left-success shadow h-100 py-2">
-                <div class="card-body">
-                    <div class="row no-gutters align-items-center">
-                        <div class="col mr-2">
-                            <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
-                                Siswa</div>
-                            <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $siswa->count() }}</div>
-                        </div>
-                        <div class="col-auto">
-                            <i class="fas fa-user-graduate fa-2x text-gray-300"></i>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <!-- Galeri Card Example -->
-        <div class="col-xl-3 col-md-6 mb-4">
-            <div class="card border-left-info shadow h-100 py-2">
-                <div class="card-body">
-                    <div class="row no-gutters align-items-center">
-                        <div class="col mr-2">
-                            <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Galeri
+        <div class="row">
+            <!-- Kelas Card Example -->
+            <div class="col-xl-3 col-md-6 mb-4">
+                <div class="card border-left-primary shadow h-100 py-2">
+                    <div class="card-body">
+                        <div class="row no-gutters align-items-center">
+                            <div class="col mr-2">
+                                <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                                    Kelas</div>
+                                <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $kelas->count() }}</div>
                             </div>
-                            <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $galeri->count() }}</div>
+                            <div class="col-auto">
+                                <i class="fas fa-house fa-2x text-gray-300"></i>
+                            </div>
                         </div>
-                        <div class="col-auto">
-                            <i class="fas fa-images fa-2x text-gray-300"></i>
+                    </div>
+                </div>
+            </div>
+            <!-- Siswa Card Example -->
+            <div class="col-xl-3 col-md-6 mb-4">
+                <div class="card border-left-success shadow h-100 py-2">
+                    <div class="card-body">
+                        <div class="row no-gutters align-items-center">
+                            <div class="col mr-2">
+                                <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
+                                    Siswa</div>
+                                <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $siswa->count() }}</div>
+                            </div>
+                            <div class="col-auto">
+                                <i class="fas fa-user-graduate fa-2x text-gray-300"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- Galeri Card Example -->
+            <div class="col-xl-3 col-md-6 mb-4">
+                <div class="card border-left-info shadow h-100 py-2">
+                    <div class="card-body">
+                        <div class="row no-gutters align-items-center">
+                            <div class="col mr-2">
+                                <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Galeri
+                                </div>
+                                <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $galeri->count() }}</div>
+                            </div>
+                            <div class="col-auto">
+                                <i class="fas fa-images fa-2x text-gray-300"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- Post Card Example -->
+            <div class="col-xl-3 col-md-6 mb-4">
+                <div class="card border-left-warning shadow h-100 py-2">
+                    <div class="card-body">
+                        <div class="row no-gutters align-items-center">
+                            <div class="col mr-2">
+                                <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">
+                                    Post</div>
+                                <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $posts->count() }}</div>
+                            </div>
+                            <div class="col-auto">
+                                <i class="fas fa-comments fa-2x text-gray-300"></i>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-
-        <!-- Post Card Example -->
-        <div class="col-xl-3 col-md-6 mb-4">
-            <div class="card border-left-warning shadow h-100 py-2">
-                <div class="card-body">
-                    <div class="row no-gutters align-items-center">
-                        <div class="col mr-2">
-                            <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">
-                                Post</div>
-                            <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $posts->count() }}</div>
+        <div class="row">
+            <!-- Data Aspek Card -->
+            <div class="col-xl-3 col-md-6 mb-4">
+                <div class="card border-left-primary shadow h-100 py-2">
+                    <div class="card-body">
+                        <div class="row no-gutters align-items-center">
+                            <div class="col mr-2">
+                                <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                                    Data Aspek</div>
+                                <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $aspek }}</div>
+                            </div>
+                            <div class="col-auto">
+                                <i class="fas fa-chart-bar fa-2x text-gray-300"></i>
+                            </div>
                         </div>
-                        <div class="col-auto">
-                            <i class="fas fa-comments fa-2x text-gray-300"></i>
+                    </div>
+                </div>
+            </div>
+            <!-- Data Kriteria Card -->
+            <div class="col-xl-3 col-md-6 mb-4">
+                <div class="card border-left-success shadow h-100 py-2">
+                    <div class="card-body">
+                        <div class="row no-gutters align-items-center">
+                            <div class="col mr-2">
+                                <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
+                                    Data Kriteria</div>
+                                <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $kriteria }}</div>
+                            </div>
+                            <div class="col-auto">
+                                <i class="fas fa-chart-bar fa-2x text-gray-300"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- Data Penilaian Card -->
+            <div class="col-xl-3 col-md-6 mb-4">
+                <div class="card border-left-info shadow h-100 py-2">
+                    <div class="card-body">
+                        <div class="row no-gutters align-items-center">
+                            <div class="col mr-2">
+                                <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Data Penilaian
+                                </div>
+                                <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $penilaian }}</div>
+                            </div>
+                            <div class="col-auto">
+                                <i class="fas fa-chart-bar fa-2x text-gray-300"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- Data Perhitungan Card -->
+            <div class="col-xl-3 col-md-6 mb-4">
+                <div class="card border-left-warning shadow h-100 py-2">
+                    <div class="card-body">
+                        <div class="row no-gutters align-items-center">
+                            <div class="col mr-2">
+                                <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">
+                                    Data Perhitungan</div>
+                                <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $penilaian }}</div>
+                            </div>
+                            <div class="col-auto">
+                                <i class="fas fa-chart-bar fa-2x text-gray-300"></i>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
+        <div class="row">
+            <!-- Data Hasil Akhir Card -->
+            <div class="col-xl-3 col-md-6 mb-4">
+                <div class="card border-left-primary shadow h-100 py-2">
+                    <div class="card-body">
+                        <div class="row no-gutters align-items-center">
+                            <div class="col mr-2">
+                                <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                                    Data Hasil Akhir</div>
+                                <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $hasilAkhir }}</div>
+                            </div>
+                            <div class="col-auto">
+                                <i class="fas fa-chart-bar fa-2x text-gray-300"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- Data Siswa Alumni Card -->
+            <div class="col-xl-3 col-md-6 mb-4">
+                <div class="card border-left-success shadow h-100 py-2">
+                    <div class="card-body">
+                        <div class="row no-gutters align-items-center">
+                            <div class="col mr-2">
+                                <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
+                                    Siswa Alumni</div>
+                                <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $siswaAlumni }}</div>
+                            </div>
+                            <div class="col-auto">
+                                <i class="fas fa-chart-bar fa-2x text-gray-300"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- Data Rekapan Card -->
+            <div class="col-xl-3 col-md-6 mb-4">
+                <div class="card border-left-info shadow h-100 py-2">
+                    <div class="card-body">
+                        <div class="row no-gutters align-items-center">
+                            <div class="col mr-2">
+                                <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Data Rekapan
+                                </div>
+                                <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $hasilAkhir }}</div>
+                            </div>
+                            <div class="col-auto">
+                                <i class="fas fa-chart-bar fa-2x text-gray-300"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     @endif
 
     <!-- Content Row -->
